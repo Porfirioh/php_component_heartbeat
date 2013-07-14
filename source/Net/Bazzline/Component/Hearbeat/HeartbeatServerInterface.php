844896dc6be759f0518d328e1c07fc0a92b75cb5
stevleibelt <stev@leibelt.de>
2013-07-14 23:10:54 +0200

    Finished heartbeat server interface

--- source/Net/Bazzline/Component/Hearbeat/HeartbeatServerInterface.php
+++ source/Net/Bazzline/Component/Hearbeat/HeartbeatServerInterface.php
@@ -14,5 +14,22 @@ namespace Net\Bazzline\Component\Heartbeat;
  */
 interface HeartbeatServerInterface
 {
+    /**
+     * Adds a client to the observer
+     *
+     * @param HeartbeatClientInterface $client
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-14
+     */
     public function addClient(HeartbeatClientInterface $client);
+
+    /**
+     * Listen to each added client. Implement handling of error here.
+     *
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-14
+     */
+    public function listen();
 }
