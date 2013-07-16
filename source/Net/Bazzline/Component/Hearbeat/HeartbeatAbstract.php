d7b03b49be8bebcbab9672f5a364ab0ce02a9ba4
stevleibelt <stev@leibelt.de>
2013-07-17 00:06:01 +0200

    Updated phpdoc

--- source/Net/Bazzline/Component/Hearbeat/HeartbeatAbstract.php
+++ source/Net/Bazzline/Component/Hearbeat/HeartbeatAbstract.php
@@ -8,8 +8,13 @@ namespace Net\Bazzline\Component\Heartbeat;
 
 /**
  * Class HeartbeatAbstract.
+ * This class is a heartbeat with benefits. You can use the runtime information
+ *  to write statistic data in your beat method implementation.
+ *
  * You have to implement the knock method. The implementation depends on your client.
- * If you need to call a url or what ever.
+ *  If you need to call a url or what ever.
+ * You have to implement the beat method.
+ * You have to implement the handleHeartAttack method.
  *
  * @package Net\Bazzline\Component\Heartbeat
  * @author stev leibelt <artodeto@arcor.de>
