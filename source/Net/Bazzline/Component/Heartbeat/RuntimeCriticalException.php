9ac48151344b599a99e085338c13931171083a4f
stevleibelt <stev@leibelt.de>
2013-07-17 17:07:36 +0200

    Created two new exceptions. Should be used and thrown in heartbeat->knock

---
+++ source/Net/Bazzline/Component/Heartbeat/RuntimeCriticalException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-17
+ */
+
+namespace Net\Bazzline\Component\Heartbeat;
+
+/**
+ * Class RuntimeCriticalException
+ *
+ * @package Net\Bazzline\Component\Heartbeat
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-17
+ */
+class RuntimeCriticalException extends RuntimeException {}
