dfe03609969973b8a5dce463d0f54ba3e5873cac
stevleibelt <stev@leibelt.de>
2013-07-17 16:36:06 +0200

    Created runtime exception

---
+++ source/Net/Bazzline/Component/Heartbeat/HeartbeatRuntimeException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-17
+ */
+
+namespace Net\Bazzline\Component\Heartbeat;
+
+use RuntimeException;
+
+/**
+ * Class HeartbeatRuntimeException
+ *
+ * @package Net\Bazzline\Component\Heartbeat
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-17
+ */
+class HeartbeatRuntimeException extends RuntimeException {}
