320a2e9cdbb4af36029bbca6d085aa27382352b8
stevleibelt <stev@leibelt.de>
2013-07-17 16:41:35 +0200

    Added exception

---
+++ source/Net/Bazzline/Component/Heartbeat/HeartbeatMonitorInvalidArgumentException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-17
+ */
+
+namespace Net\Bazzline\Component\Heartbeat;
+
+use InvalidArgumentException;
+
+/**
+ * Class HeartbeatMonitorInvalidArgumentException
+ *
+ * @package Net\Bazzline\Component\Heartbeat
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-17
+ */
+class HeartbeatMonitorInvalidArgumentException extends InvalidArgumentException {}
