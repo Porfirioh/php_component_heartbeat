84f2b08e7cb96536e225b25fd6606c8ddea73f89
stevleibelt <stev@leibelt.de>
2013-07-22 21:56:07 +0200

    copied monitor

---
+++ example/Example/JSONAndFork/Monitor.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-22
+ */
+
+namespace Example\JSONAndFork;
+
+use Net\Bazzline\Component\Heartbeat\HeartbeatMonitor;
+
+/**
+ * Class Monitor
+ *
+ * @package Example\JSONBasedImplementation
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-22
+ */
+class Monitor extends HeartbeatMonitor {}
