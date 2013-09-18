2b3a89cf299bf0e958df49043bac4933bfec26f3
stevleibelt <stev@leibelt.de>
2013-09-18 20:57:42 +0200

    updated interface

--- source/Net/Bazzline/Component/Heartbeat/TimestampInterface.php
+++ source/Net/Bazzline/Component/Heartbeat/TimestampInterface.php
@@ -16,25 +16,38 @@ namespace Net\Bazzline\Component\Heartbeat;
 interface TimestampInterface 
 {
     /**
-     * @return mixed
+     * @return int
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-18
+     */
+    public function getCurrentTimestamp();
+
+    /**
+     * @return int
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-09-19
      */
     public function getInitialTimestamp();
 
     /**
-     * @param null $time
+     * Returns difference between provided timestamp and initial timestamp.
+     * If null is provided, the current timestamp is used.
+     *
+     * @param null|int $time
+     * @return mixed
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-18
+     */
+    public function getTimestampDifference($time = null);
+
+    /**
+     * Adds manipulation for initial timestamp.
+     * If null is provided, the current timestamp is used.
+     *
+     * @param null|int $time
      * @return mixed
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-09-19
      */
     public function setInitialTimestamp($time = null);
-
-    /**
-     * @param null $time
-     * @return mixed
-     * @author stev leibelt <artodeto@arcor.de>
-     * @since 2013-09-18
-     */
-    public function getTimeDifference($time = null);
 }
