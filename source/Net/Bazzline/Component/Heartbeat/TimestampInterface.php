e9a1b4739d1159e35c86b470f0c6a84d08023a47
stevleibelt <stev@leibelt.de>
2013-09-18 20:52:12 +0200

    Finished first draft of timestamp interface

--- source/Net/Bazzline/Component/Heartbeat/TimestampInterface.php
+++ source/Net/Bazzline/Component/Heartbeat/TimestampInterface.php
@@ -15,4 +15,26 @@ namespace Net\Bazzline\Component\Heartbeat;
  */
 interface TimestampInterface 
 {
+    /**
+     * @return mixed
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-19
+     */
+    public function getInitialTimestamp();
+
+    /**
+     * @param null $time
+     * @return mixed
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-19
+     */
+    public function setInitialTimestamp($time = null);
+
+    /**
+     * @param null $time
+     * @return mixed
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-18
+     */
+    public function getTimeDifference($time = null);
 }
