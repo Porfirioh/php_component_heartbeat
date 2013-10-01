de66998f1f95ac12250f982ef545bd23651da12c
stevleibelt <stev@leibelt.de>
2013-10-01 20:54:57 +0200

    Scribbled HeartbeatClientHistoryEventInterface

---
+++ source/Net/Bazzline/Component/Heartbeat/HeartbeatClientHistoryEventInterface.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-10-01
+ */
+
+namespace Net\Bazzline\Component\Heartbeat;
+
+/**
+ * Class HeartbeatClientHistoryEventInterface
+ *
+ * @package Net\Bazzline\Component\Heartbeat
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-10-01
+ */
+interface HeartbeatClientHistoryEventInterface
+{
+    /**
+     * @return string
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-01
+     */
+    public function getIdentifier();
+
+    /**
+     * @return string
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-01
+     */
+    public function getName();
+
+    /**
+     * @return string
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-01
+     */
+    public function getStatus();
+
+    /**
+     * @return int
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-01
+     */
+    public function getTimestamp();
+
+    /**
+     * @param string $identifier
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-01
+     */
+    public function setIdentifier($identifier);
+
+    /**
+     * @param string $name
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-01
+     */
+    public function setName($name);
+
+    /**
+     * @param null|RuntimeException $exception
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-01
+     */
+    public function setStatusByException(RuntimeException $exception = null);
+
+    /**
+     * @param null|string $timestamp
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-10-01
+     */
+    public function setTimestamp($timestamp = null);
+}
